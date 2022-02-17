525b6405c8ee97481b56c53d92574229b75f1595
wenodev <logicalstark1105@gmail.com>
2022-02-17 16:48:51 +0900

    initial commit

---
+++ src/main/java/com/example/firtcollection/FirtCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.firtcollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirtCollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirtCollectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/firtcollection/FirtCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firtcollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirtCollectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
